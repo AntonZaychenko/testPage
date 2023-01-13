0f88d31d065db8e08852ac893596a250df4a3771
AntonZaychenko <105227367+AntonZaychenko@users.noreply.github.com>
2023-01-13 13:00:42 +0200

    first test

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+export const Header = () => {
+    return (
+        <header>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </header>
+    )
+}
